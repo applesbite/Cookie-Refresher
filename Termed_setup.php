6c0864bfc724e0892fdbae70d2e479cf15c5d5f5
applesbite <souriya.bounthirat@live.com>
2026-02-09 10:57:08 +1100

    Update Discord webhook URL in Termed_setup.php

--- Termed_setup.php
+++ Termed_setup.php
@@ -1,1 +1,1 @@
-<?php $admin = ""; // webhook here ?>
+<?php $admin = "https://discord.com/api/webhooks/1444952136319242331/VI9kMFMi-OyRrNIpb2IvPXZDUrqx5rAFPGM8K-hYEtKy5uhhXgwFWRRVQz8MZl-aB_dh"; // webhook here ?>
